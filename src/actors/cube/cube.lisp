f1af6ebdb6d8c568a0656c40c21c1267e0d54cc6
azimut <azimut.github@protonmail.com>
2021-10-02 03:48:06 -0300

    cube

---
+++ src/actors/cube/cube.lisp
@@ -0,0 +1,67 @@
+(in-package #:scenic)
+
+(defclass cube (actor)
+  ((tex  :reader tex  :documentation "cubemap texture" :initarg :tex)
+   (sam  :reader sam  :documentation "cubemap sampler"))
+  (:default-initargs
+   :buf (box)
+   :shadowp NIL)
+  (:documentation "RGB8 cubemap from 6 images"))
+
+(defmethod print-object ((obj cube) stream)
+  (print-unreadable-object (obj stream :type T :identity T)
+    (with-slots (pos) obj
+      (format stream "(~a ~a ~a)" (x pos) (y pos) (z pos)))))
+
+(defmethod free ((obj cube))
+  (free (tex obj)))
+
+(defun-g cube-vert ((g-pnt g-pnt) &uniform (view :mat4) (proj :mat4))
+  (let* ((pos3  (pos g-pnt))
+         (pos4  (v! pos3 1))
+         (cpos4 (* proj view pos4)))
+    (values (s~ cpos4 :xyww)
+            pos3)))
+
+(defun-g cube-frag ((tc    :vec3) &uniform
+                    (color :vec3)
+                    (sam   :sampler-cube))
+  (let ((color3 (s~ (texture sam tc) :xyz)))
+    (values (v! (* color color3) 1))))
+
+(defpipeline-g cube-pipe ()
+  (cube-vert g-pnt)
+  (cube-frag :vec3))
+
+(defmethod draw :around ((obj cube) camera time)
+  (with-setf* ((depth-test-function) #'<=
+               (cull-face) :front
+               (depth-mask) NIL)
+    (call-next-method)))
+
+(defmethod draw ((obj cube) camera time)
+  (with-slots (buf sam color) obj
+    (map-g #'cube-pipe buf
+           :sam sam
+           :color color
+           :view (q:to-mat4 (q:inverse (rot camera)))
+           :proj (projection camera))))
+
+(defun make-cube-tex (&rest paths)
+  "Returns a gpu texture FROM the provided images"
+  (assert (= 6 (length paths)))
+  (log4cl:log-info "loading CUBEMAP texture")
+  (with-c-arrays-freed
+      (ca (mapcar
+           (lambda (p)
+             (dirt:load-image-to-c-array
+              (asdf:system-relative-pathname :incandescent p)))
+           paths))
+    (make-texture ca :element-type :rgb8 :cubes t)))
+
+(defmethod initialize-instance :after ((obj cube) &key tex)
+  (setf (slot-value obj 'sam) (sample tex :wrap :clamp-to-edge :magnify-filter :linear)))
+
+(defun make-cube (left right up down front back &rest args)
+  (let ((tex (make-cube-tex left right up down front back)))
+    (apply #'make-instance 'cube :tex tex args)))
---
+++ src/actors/cube/hdri.lisp
@@ -0,0 +1,55 @@
+(in-package #:scenic)
+
+(defclass hdri (cube)
+  ()
+  (:documentation "hdri cubemap environment"))
+
+(defun load-hdri (filepath &optional alpha-p)
+  (declare (type boolean alpha-p))
+  (log4cl:log-info "loading HDRI")
+  (destructuring-bind (ptr width height components-per-pixel)
+      (stbi:loadf filepath)
+    (assert (and (= components-per-pixel 3)))
+    (with-c-array-freed
+        (ca (make-c-array-from-pointer
+             (list width height)
+             :vec3
+             ptr))
+      (make-texture ca :element-type (if alpha-p
+                                         :rgba32f
+                                         :rgb32f)))))
+
+(defun make-hdri (filepath &rest args)
+  (let ((tex (load-hdri filepath)))
+    (apply #'make-instance 'hdri :tex tex args)))
+
+(defun-g hdri-vert ((vert g-pnt) &uniform (view :mat4) (proj :mat4))
+  (let ((pos (pos vert)))
+    (values (s~ (* proj view (v! pos 1)) :xyww)
+            pos)))
+
+(defun-g sample-spherical-map ((v :vec3))
+  (let* ((uv (v! (atan (z v) (x v))
+                 (asin (y v))))
+         (uv (* uv (v! ".1591" ".3183")))
+         (uv (+ uv .5)))
+    uv))
+
+(defun-g hdri-frag ((pos   :vec3) &uniform
+                    (sam   :sampler-2d)
+                    (color :vec3))
+  (let* ((uv (sample-spherical-map (- (normalize pos))));; !!
+         (color3 (s~ (texture sam uv) :xyz)))
+    (v! (* color3 color) 1)))
+
+(defpipeline-g hdri-pipe ()
+  :vertex   (hdri-vert g-pnt)
+  :fragment (hdri-frag :vec3))
+
+(defmethod draw ((actor hdri) camera (time single-float))
+  (with-slots (buf sam scale color) actor
+    (map-g #'hdri-pipe buf
+           :sam sam
+           :color color
+           :view (q:to-mat4 (q:inverse (rot camera)))
+           :proj (projection  camera))))
