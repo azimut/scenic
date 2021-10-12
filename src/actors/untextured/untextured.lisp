b9be497395e84f92e5128749e39748e1e0672abb
azimut <azimut.github@protonmail.com>
2021-10-12 16:21:25 -0300

    untextured constructs accept keys

--- src/actors/untextured/untextured.lisp
+++ src/actors/untextured/untextured.lisp
@@ -4,12 +4,12 @@
   ()
   (:documentation "pbr untextured"))
 
-(defun make-box (&optional (w 1f0) (h 1f0) (d 1f0) &rest initargs)
+(defun make-box (&rest initargs &key (w 1f0) (h 1f0) (d 1f0) &allow-other-keys)
   (apply #'make-instance 'untextured :buf (box w h d) initargs))
-(defun make-sphere (&optional (radius 1f0) (lat 30) (long 30) &rest initargs)
+(defun make-sphere (&rest initargs &key (radius 1f0) (lat 30) (long 30) &allow-other-keys)
   (apply #'make-instance 'untextured :buf (sphere radius lat long) initargs))
-(defun make-cylinder (&optional (radius 0.5) (height 1f0) &rest initargs)
+(defun make-cylinder (&rest initargs &key (radius 0.5) (height 1f0) &allow-other-keys)
   (apply #'make-instance 'untextured :buf (cylinder radius height) initargs))
-(defun make-cone (&optional (radius 1f0) (height 1f0) &rest initargs)
+(defun make-cone (&rest initargs &key (radius 1f0) (height 1f0) &allow-other-keys)
   (apply #'make-instance 'untextured :buf (cone radius height) initargs))
 
