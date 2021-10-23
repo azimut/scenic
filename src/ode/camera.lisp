444005235802a8365c528316295f7560964394e9
azimut <azimut.github@protonmail.com>
2021-10-22 21:28:56 -0300

    ode: double->float, refactor camera

--- src/ode/camera.lisp
+++ src/ode/camera.lisp
@@ -1,58 +1,33 @@
 (in-package #:scenic)
 
-;; Attach physics to a camera...
-
-(defvar *camera-solid* nil)
-
 (defclass physic-camera (pers)
-  ((body    :initarg :body :documentation "body pointer")
-   (mass    :initarg :mass :documentation "mass pointer")
-   (geom    :initarg :geom :documentation "geometry pointer"))
+  ((body    :reader body    :initarg :body)
+   (geom    :reader geom    :initarg :geom)
+   (mass    :reader mass    :initarg :mass); FIXME: Leaking?
+   (space   :reader space   :initarg :space)
+   (radius  :reader radius  :initarg :radius)
+   (density :reader density :initarg :density))
   (:default-initargs
+   :radius .5f0
+   :density 1f0
    :body (%ode:body-create *world*)
-   :mass (cffi:foreign-alloc '%ode:mass)))
+   :mass (cffi:foreign-alloc '%ode:mass)
+   :space (space (current-scene))))
 
-;; FIXME: mass is leaking
-(defmethod free ((object physic-camera))
-  (with-slots (body geom) object
-    (%ode:body-destroy body)
-    (%ode:geom-destroy geom)))
+(defmethod free ((obj physic-camera))
+  (%ode:body-destroy (body obj))
+  (%ode:geom-destroy (geom obj)))
 
-(defmethod initialize-instance :after ((obj physic-camera) &key)
-  (let ((radius .5f0)
-        (density 1f0))
-    (with-slots (mass body geom pos) obj
-      (setf geom (%ode:create-sphere *space* radius))
-      (cffi-c-ref:c-let ((m %ode:mass :from mass))
-        (%ode:mass-set-sphere (m &) density radius)
-        (%ode:body-set-mass body (m &))
-        (%ode:geom-set-body geom body))
-      ;; trigger update on ODE side
-      (setf (pos obj) pos))))
+(defmethod initialize-instance :after ((obj physic-camera) &key space radius density mass body pos)
+  (with-slots (geom) obj
+    (setf geom (%ode:create-sphere space radius))
+    (cffi-c-ref:c-let ((m %ode:mass :from mass))
+      (%ode:mass-set-sphere (m &) density radius)
+      (%ode:body-set-mass body (m &))
+      (%ode:geom-set-body geom body)))
+  (setf (pos obj) pos))
 
-(defmethod (setf pos) (value (object physic-camera))
-  (%ode:body-set-position (slot-value object 'body)
-                          (x value)
-                          (y value)
-                          (z value))
-  (setf (slot-value object 'pos) value))
+(defmethod (setf pos) :after (new-value (obj physic-camera))
+  (%ode:body-set-position (body obj) (x new-value) (y new-value) (z new-value)))
 
-(defmethod update :before ((camera physic-camera) dt)
-  (with-slots (pos) camera
-    (%ode:body-set-position (slot-value camera 'body)
-                            (x pos)
-                            (y pos)
-                            (z pos))))
-
-(defun toggle-solid-camera ()
-  (if (eq 'physic-camera (serapeum:class-name-of *currentcamera*))
-      (progn
-        (camera-to-camera *currentcamera* *camera*)
-        (setf *currentcamera* *camera*)
-        (free *camera-solid*)
-        (setf *camera-solid* nil))
-      (progn
-        (setf *camera-solid* (make-instance 'physic-camera))
-        (camera-to-camera *currentcamera* *camera-solid*)
-        (setf *currentcamera* *camera-solid*)))
-  *currentcamera*)
+;; TODO: helper to swap/toggle cameras
