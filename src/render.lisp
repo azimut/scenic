8ef8723b9446685ae7ccf34ec9a9936dbd6401fb
azimut <azimut.github@protonmail.com>
2023-06-01 10:37:20 -0300

    vert shader for SSR

--- src/render.lisp
+++ src/render.lisp
@@ -5,6 +5,23 @@
 (defun-g treat-uvs ((uv :vec2))
   (v! (x uv) (- 1.0 (y uv))))
 
+(defun-g vert ((vert g-pnt) &uniform
+               (model-world :mat4)
+               (world-view  :mat4)
+               (view-clip   :mat4)
+               (scale       :float))
+  (let* ((pos        (* scale (pos vert)))
+         (norm       (norm vert))
+         (tex        (tex vert))
+         (world-norm (* (m4:to-mat3 model-world) norm))
+         (world-pos  (* model-world (v! pos 1)))
+         (view-pos   (* world-view  world-pos))
+         (clip-pos   (* view-clip   view-pos)))
+    (values clip-pos
+            tex
+            world-norm
+            (s~ world-pos :xyz))))
+
 (defun-g vert-with-tbdata ((vert g-pnt) (tb tb-data) &uniform
                            (model-world :mat4)
                            (world-view  :mat4)
