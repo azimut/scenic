0dbcf4711c0c5507d48bed829566d2c84cd19829
azimut <azimut.github@protonmail.com>
2023-06-18 12:52:18 -0300

    vxgi pair with shimmer

--- src/scene/vxgi.lisp
+++ src/scene/vxgi.lisp
@@ -14,7 +14,7 @@
                        nil
                        :dimensions '(64 64 64)
                        :mipmap 7
-                       :element-type :rgba8))
+                       :element-type :rgba16f))
     (setf voxel-sam (sample voxel-light :magnify-filter :nearest
                                         :wrap :clamp-to-border))
     (setf voxel-zam (sample voxel-light :magnify-filter :nearest
@@ -87,7 +87,7 @@
          (distance    (distance light-pos pos))
          (attenuation (/ (+ 1 (* linear distance) (* quadratic distance distance))))
          (diff        (saturate (dot (normalize nor) direction))))
-    (* diff light-color attenuation)))
+    (* diff light-color color attenuation)))
 
 ;; Scales and bias a given vector (i.e. from [-1, 1] to [0, 1])
 (defun-g scale-and-bias ((p :vec3))
@@ -197,39 +197,44 @@
   (let* ((voxel-size #.(/ 1f0 64f0))
          (mipmap-hardcap 5.4)
          (max-dist
-           (distance (abs from) (v3! -1))
+           ;;(distance (abs from) (v3! -1))
            ;;#.(sqrt 2)
-           ;;#.(sqrt 3)
+           #.(sqrt 3)
            );F 1.414213=(sqrt 2);A 1.73205080757=(sqrt 3)
          ;;
-         (direction (normalize direction))
          (aperture
-           "0.325"
-           ;;".55785173935"
-           );F .325;A .55785173935=(tan 22.5); AKA CONE_SPREAD
-         (acc (vec4 0f0))
+           ;;"0.325"
+           ;;#.(max 0.1f0 (tan (* (radians 22f0) 0.5)))
+           ".55785173935"
+           ) ;F=.325;A=.55785173935=(tan 22.5); AKA CONE_SPREAD
+         ;;
          ;; Controls bleeding from close surfaces.
          ;; Low values look rather bad if using shadow cone tracing.
          ;; Might be a better choice to use shadow maps and lower this value.
          (dist
+           ;;#.(* 1.5 (/ 1 64));; Shimmer
            "0.1953125"
            ;;(* 1 0.04)
            ;;(* 3.5f0 voxel-size)
-           ); F .1953125 ; A 0.04 * voxelgiOffset("1"*100/100)
-         (diam (* dist aperture)))
+           )        ; F .1953125 ; A 0.04 * voxelgiOffset("1"*100/100)
+         (diam (* dist aperture))
+         (direction (normalize direction))
+         (acc (vec4 0f0)))
     ;; Trace
     (while (and (< dist max-dist)
                 (< (w acc) 1f0))
-           (let* ((sample-pos (scale-and-bias (+ from (* dist direction))))
-                  (l          (+ 1f0 (/ (* aperture dist) voxel-size)))
-                  (level      (log2 l))
-                  (ll         (* (1+ level) (1+ level)))
-                  (voxel      (texture-lod voxel-light
-                                           sample-pos
-                                           (min mipmap-hardcap level))))
-             (incf acc  (* 0.075 ll voxel (pow (- 1f0 (w voxel)) 2f0)))
-             (incf dist (* ll voxel-size 2))))
-    (pow (* 2 (s~ acc :xyz))
+           (let* ((c (scale-and-bias (+ from (* direction dist))))
+                  (l (1+ (/ diam voxel-size)))
+                  (level (log2 l))
+                  (ll (* (1+ level) (1+ level)))
+                  (voxel (texture-lod voxel-light
+                                      c
+                                      (min mipmap-hardcap level))))
+             (incf acc
+                   (* 0.075 ll voxel (pow (- 1f0 (w voxel)) 2f0)))
+             (incf dist
+                   (* ll voxel-size 2))))
+    (pow (* 2f0 (s~ acc :xyz))
          (vec3 1.5))))
 
 ;; From Friduric
@@ -242,19 +247,28 @@
         (cross u (v! 0 1 0))
         (cross u v))))
 
+;; From Armory (used instead of orthogonal)
+(defun-g tangent ((n :vec3))
+  (let ((t1 (cross n (v! 0 0 1)))
+        (t2 (cross n (v! 0 1 0))))
+    (if (> (length t1) (length t2))
+        (normalize t1)
+        (normalize t2))))
+
+
 (defun-g indirect-diffuse-light ((wpos        :vec3)
                                  (normal      :vec3)
                                  (voxel-light :sampler-3d)
                                  (albedo      :vec3))
-  (let* ((isqrt2                  #.(/ (sqrt 2f0) 2f0)) ; 0.7071
-         (diffuse-indirect-factor .52)
+  (let* ((isqrt2                  #.(/ (sqrt 2) 2)) ; 0.7071
+         (diffuse-indirect-factor 0.52)
          ;; Angle mix (1.0f => orthogonal direction,
          ;;            0.0f => direction of normal).
          (angle-mix               0.5)
-         (w                       (v! 1 1 1)) ; cone weights
+         (w                       (vec3 1)) ; cone weights
          ;; Find a base for the side cones with the normal as one
          ;; of its base vectors.
-         (ortho                   (normalize (orthogonal normal)))
+         (ortho                   (normalize (tangent normal)))
          (ortho2                  (normalize (cross ortho normal)))
          ;; Find base vectors for the corner cones too.
          (corner                  (* 0.5 (+ ortho ortho2)))
@@ -300,7 +314,9 @@
                                                  (mix normal (- corner2) angle-mix)
                                                  voxel-light)))
     ;; Return result.
-    (* acc diffuse-indirect-factor (+ albedo 0.001))
+    (* diffuse-indirect-factor
+       acc
+       (+ albedo 0.001))
     ;;(* (+ albedo .0001) (/ acc 9))
     ))
 
@@ -382,7 +398,7 @@
                                 (aref fudge i)
                                 i)))))
     (v! (+ indirect
-           ;;final-color
+           final-color
            )
         ;; TODO: this alpha is to blend the possible cubemap
         ;;(- 1 (step (y color) 0f0))
