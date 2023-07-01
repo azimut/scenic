2dcbb9ac2f8f9c002b45c995396e08b7e3001759
azimut <azimut.github@protonmail.com>
2023-07-01 15:52:43 -0300

    Add deploy quit hooks

--- bin/deploy.lisp
+++ bin/deploy.lisp
@@ -6,6 +6,9 @@
 
 (deploy:define-library CL-OPENGL-BINDINGS::OPENGL :dont-deploy t)
 (deploy:define-resource-directory assets "static/")
+(deploy:define-hook (:quit scenic) ()
+  (alut:exit)
+  (cepl:quit))
 
 (sb-ext:gc :full t)
 (asdf:make :scenic :force t)
