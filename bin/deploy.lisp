f169038a0e9daf695b10778c3232a3644e915c4d
azimut <azimut.github@protonmail.com>
2023-06-30 22:54:15 -0300

    action: Fix? Windows by using "~" inside a (load)

--- bin/deploy.lisp
+++ bin/deploy.lisp
@@ -1,7 +1,11 @@
+(load "~/quicklisp/setup.lisp")
+
 (ql:quickload :deploy)
 (load "../scenic.asd")
 (ql:quickload :scenic)
+
 (deploy:define-library CL-OPENGL-BINDINGS::OPENGL :dont-deploy t)
 (deploy:define-resource-directory assets "static/")
+
 (sb-ext:gc :full t)
 (asdf:make :scenic :force t)
