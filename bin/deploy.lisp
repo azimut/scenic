34189a07ae473fa9cb93060d204c8c43fcca6d48
azimut <azimut.github@protonmail.com>
2023-07-23 22:05:49 -0300

    Inhibit asdf and quicklisp on DEPLOY build
    Mainly due I am using asdf:relative-path.
    It saves me a couple of seconds on start.
    
    https://github.com/OpenBookStore/openbookstore/blob/4011f987eb9fa2b45485a2e69684ca57166547d9/bookshops.asd#L208
    https://github.com/fare/workout-timer/blob/master/finalizer.lisp#L13
    https://github.com/Shirakumo/radiance/blob/1c28d0660c5af528a57de235448713e3c6db837e/deploy.lisp#L23

--- bin/deploy.lisp
+++ bin/deploy.lisp
@@ -10,5 +10,13 @@
   (alut:exit)
   (cepl:quit))
 
+(deploy:define-hook (:deploy asdf) (directory)
+  (declare (ignorable directory))
+  #+asdf (mapc #'asdf:register-immutable-system (asdf:already-loaded-systems))
+  #+asdf (setf asdf:*central-registry* NIL)
+  #+quicklisp (setf ql:*local-project-directories* ())
+  #+asdf (asdf:clear-source-registry)
+  #+asdf (defun asdf:upgrade-asdf () nil))
+
 (sb-ext:gc :full t)
 (asdf:make :scenic :force t)
