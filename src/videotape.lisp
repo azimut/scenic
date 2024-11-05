343ad10a494c23dbf67f1bc313d465f9216704fd
azimut <azimut.github@protonmail.com>
2024-11-05 01:55:54 -0300

    fix: videotape keep size even on resizes, fxaa3 output
    use a custom viewport to keep size

--- src/videotape.lisp
+++ src/videotape.lisp
@@ -5,7 +5,7 @@
 
 ;; TODO: .rocket filepath
 (defclass videotape (renderable)
-  ((process  :reader  process)
+  ((process  :reader  videotape-process)
    (duration :initarg :duration :documentation "duration in seconds" :reader duration)
    (fps      :initarg :fps      :documentation "frames per second" :reader fps)
    (filename :initarg :filename :documentation "output video filename"))
@@ -13,8 +13,7 @@
    :duration 10
    :filename "output.mp4"
    :fps 30
-   :dim (viewport-dimensions (current-viewport))
-   :texture-opts '((0 :element-type :rgb8)) ;; CEPL names rgb24=rgb8?
+   :texture-opts '((0 :element-type :rgb8)) ;; LDR - CEPL names rgb24=rgb8?
    :sample-opts '((:wrap :clamp-to-edge)))
   (:documentation "records *STATE* into video FILENAME"))
 
@@ -38,32 +37,35 @@
              :error-output nil)))))
 
 (defmethod free ((obj videotape))
-  (uiop:close-streams     (process obj))
-  (uiop:terminate-process (process obj))
-  (uiop:wait-process      (process obj)))
+  (uiop:close-streams     (videotape-process obj))
+  (uiop:terminate-process (videotape-process obj))
+  (uiop:wait-process      (videotape-process obj)))
 
 (defun record-loop ()
   (unless *rocket* (return-from record-loop))
   ;; scene -> tape
   (map-g-into (fbo *tape*)
-              #'hdr-acesfilm-pipe (bs *state*)
-              :sam (first (sam (prev *state*)))
-              :exposure .9)
+              #'fxaa3-pipe (bs *state*)
+              :one-over-res (v2:/ (v! 1 1) (viewport-resolution (current-viewport)))
+              :settings (v! 0.75 0.166 0.0833)
+              :sam (first (sam (prev *state*))))
   ;;  tape -> ffmpeg
   (let ((c-array (pull1-g (first (tex *tape*)))))
     (destructuring-bind (width height) (dim *tape*)
       (loop :for i :from (1- (* width height)) :downto 0 :do
         (write-sequence
          (row-major-aref-c c-array i)
-         (uiop:process-info-input (process *tape*))))))
+         (uiop:process-info-input (videotape-process *tape*))))))
   ;; TICK
   (let ((step-size (/ (rocketman::state-rps *rocket*) (fps *tape*))));; TODO: rps?
     (incf (rocketman::state-row *rocket*)
           step-size)))
 
-(defun record-init ()
-  (setf (viewport-dimensions (current-viewport)) (dim *tape*))
+(defun record-init (&aux (width  (first  (dim *tape*))) (height (second (dim *tape*))))
   (init)
+  ;; Once we have an event-loop, ask for prev/next to resize
+  (issue (current-scene) 'resize :width width :height height)
+  (main-loop)
   (main-loop)); with an extra draw we make sure everything is ready (eg: ibl)
 
 (def-simple-main-loop record-render (:on-start #'record-init)
@@ -83,5 +85,7 @@
 
 (defun record (&key (fps 30) (duration 1) (filename "output.mp4") (width 800) (height 600))
   (with-videotape (fps duration filename width height)
-    (record-render :stop)
-    (record-render :start (* fps duration))))
+    (with-viewport (make-viewport `(,width ,height))
+      (record-render :stop)
+      (record-render :start (* fps duration))))
+  (skitter-cleanup))
